44b162df0e86dab67875a8695f6d119fb9e52f28
Victor Yu <159395423+vy038@users.noreply.github.com>
2024-11-29 16:46:04 -0500

    Add files via upload

--- importData.py
+++ importData.py
@@ -1,4 +1,4 @@
 import csv
-with open('students.csv', 'r') as read_obj: # read csv file as a list of lists
+with open('microbit.csv', 'r') as read_obj: # read csv file as a list of lists
   csv_reader = csv.reader(read_obj) # pass the file object to reader() to get the reader object
   list_of_rows = list(csv_reader) # Pass reader object to list() to get a list of lists
