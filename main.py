9cd916e29a1d9a822019b6565054f256fa4b1599
AnshRoutray <anshroutray01@gmail.com>
2024-11-29 11:02:43 -0500

    Pose Detection Commit

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
 import cv2 as cv
+from cvzone.PoseModule import PoseDetector
+
+cap = cv.VideoCapture("Form1.mp4")
+
+detector = PoseDetector()
+
+while True:
+    success, img = cap.read()
+    img = detector.findPose(img)
+    lmList, bboxinfo = detector.findPosition(img)
+    cv.imshow("Image", img)
+    cv.waitKey(1)
