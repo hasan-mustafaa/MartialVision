c231cda947bb3b5ccb2b5d966bc6d068b9263b32
Victor Yu <159395423+vy038@users.noreply.github.com>
2024-11-29 17:31:50 -0500

    Add files via upload

--- importData.py
+++ importData.py
@@ -1,4 +1,8 @@
 import csv
-with open('microbit.csv', 'r') as read_obj: # read csv file as a list of lists
+with open('leftGlove.csv', 'r') as read_obj: # read csv file as a list of lists
   csv_reader = csv.reader(read_obj) # pass the file object to reader() to get the reader object
-  list_of_rows = list(csv_reader) # Pass reader object to list() to get a list of lists
+  leftGloveData = list(csv_reader) # Pass reader object to list() to get a list of lists
+
+with open('rightGlove.csv', 'r') as read_obj: # read csv file as a list of lists
+  csv_reader = csv.reader(read_obj) # pass the file object to reader() to get the reader object
+  rightGloveData = list(csv_reader) # Pass reader object to list() to get a list of lists
