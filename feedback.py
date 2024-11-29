a449c74afb7f5372207b119dad2553dc3587f27b
AnshRoutray <anshroutray01@gmail.com>
2024-11-29 17:03:39 -0500

    commit

--- feedback.py
+++ feedback.py
@@ -3,7 +3,16 @@ from video_analyze import detect_rest_positions
 
 # Step 1: Analyze "Form1.mp4"cl
 video_file = "Form1.mp4"
-rest_positions_form1 = detect_rest_positions(video_file)
+video_positions = detect_rest_positions(video_file)
 
 video_file = "Demo2.mp4"
-rest_positions_form1 = detect_rest_positions(video_file)
+user_positions = detect_rest_positions(video_file)
+
+feedback = ""
+
+if len(video_positions) > len(user_positions):
+    feedback = "You are doing it too quickly, please be more firm"
+elif len(video_positions) < len(user_positions):
+    feedback = "Too sluggish, be quicker"
+else:
+    for index in range(1, len(video_positions)):
